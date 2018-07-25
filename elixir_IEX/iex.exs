64d0b9a0fa362de64f8ddf4a920478d09df071eb
Eduardo Borsa <borsa.dado@gmail.com>
2018-07-25 08:51:20 -0300

    Added Elixir Terminal config
    Signed-off-by: Eduardo Borsa <borsa.dado@gmail.com>

---
+++ elixir_IEX/iex.exs
@@ -0,0 +1,19 @@
+# ~/.iex.exs
+Application.put_env(:elixir, :ansi_enabled, true)
+
+IEx.configure(
+  colors: [enabled: true],
+  default_prompt:
+    [
+      # ANSI CHA, move cursor to column 1
+      "\e[G",
+      :yellow,
+      # IEx prompt variable
+      "%prefix",
+      # plain string
+      ">",
+      :reset
+    ]
+    |> IO.ANSI.format()
+    |> IO.chardata_to_string()
+)
